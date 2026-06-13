b30382043ff579c013de5c37dbf9ce8cb42c13ef
Davi <daviaika07@gmail.com>
2026-06-12 22:00:26 -0300

    chore: create initial backend package structure

---
+++ src/main/java/davi/spf/supportflow/SupportFlowApplication.java
@@ -0,0 +1,13 @@
+package davi.spf.supportflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportFlowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SupportFlowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/davi/spf/supportflow/SupportFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package davi.spf.supportflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupportFlowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
